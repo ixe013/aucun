33d73f328023f4eb099f0cbf4be1e3a95cd62caf
Guillaume <guillaume@paralint.com>
2008-09-19 03:12:52 +0000

    Script to automate creation of binary and source zip files

--- makezip.cmd
+++ makezip.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
-msbuild AnyUserUnlockGina.sln /nologo /p:Configuration=Release /t:Rebuild
-msbuild AnyUserUnlockGina.sln /nologo /p:Configuration=Debug   /t:Rebuild
+msbuild AnyUserUnlockGina.sln /nologo /v:m /p:Configuration=Release /t:Rebuild
+msbuild AnyUserUnlockGina.sln /nologo /v:m /p:Configuration=Debug   /t:Rebuild
 
 SET TEMP_FILE=%RANDOM%-%RANDOM%.tmp
 svn info | findstr URL | gawk '{print $2}' > %TEMP_FILE%
@@ -9,11 +9,14 @@ svn info | findstr URL | gawk '{print $2}' > %TEMP_FILE%
 SET /P SVN_URL= < %TEMP_FILE%
 del %TEMP_FILE%
 
-svn co %SVN_URL% aucun
+svn co -q %SVN_URL% aucun
 
-zip -rp aucun-src.zip aucun\*
+del aucun-src.zip
+del aucun.zip
+
+zip -rp -q aucun-src.zip aucun\*
 rd /s /q aucun 
 
-zip -j aucun.zip README.txt release\aucun.dll sample.reg
+zip -j -q aucun.zip README.txt release\aucun.dll sample.reg
 
 endlocal
