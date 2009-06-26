ef990f8889741b125bdc1daf6ef998d09e8edd2f
Guillaume <guillaume@paralint.com>
2009-06-26 02:29:10 +0000

    First files for saving the last user name

---
+++ GINA/registry_setup.cpp
@@ -0,0 +1,15 @@
+#include <windows.h>
+#include "registry_setup.h"
+
+BOOL SetupSelfserveRegistry()
+{
+	BOOL result = FALSE;
+	return result;
+}
+
+BOOL TearDownSelfserveRegistry()
+{
+	BOOL result = FALSE;
+	return result;
+}
+
---
+++ GINA/registry_setup.h
@@ -0,0 +1,9 @@
+#ifndef __REGISTRY_SETUP_H__
+#define __REGISTRY_SETUP_H__
+
+#include "extern.h"
+
+EXTERN BOOL SetupSelfserveRegistry();
+EXTERN BOOL TearDownSelfserveRegistry();
+
+#endif
