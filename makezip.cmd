ec60ef6f7c2c0f3825b3efb6229065e4e6fcf6dc
Guillaume <guillaume@paralint.com>
2009-11-25 04:42:06 +0000

    Released 1.4.5

--- makezip.cmd
+++ makezip.cmd
@@ -5,10 +5,11 @@ setlocal
 SET PROJECT_NAME=aucun
 
 echo Zipping versioned project files
+if exist %PROJECT_NAME%.zip del %PROJECT_NAME%.zip
 if exist %PROJECT_NAME%-src.zip del %PROJECT_NAME%-src.zip
 svn st -v | findstr /V /B "[\?CDIX\!\~]" | gawk "{ $0 = substr($0, 6); print $4 }" | zip %PROJECT_NAME%-src.zip -@ 
 
-
+echo.
 echo Preparing for build
 md %PROJECT_NAME%
 
@@ -16,11 +17,20 @@ pushd %PROJECT_NAME%
 
 unzip -q ..\%PROJECT_NAME%-src.zip
 
+echo.
+findstr /s /n DebugBreak *.c *.cpp *.h 
+if ERRORLEVEL 1 (
 echo Building...
 vcbuild /nologo
 
 echo Creating binary zip
 zip -j -q ..\%PROJECT_NAME%.zip README.txt release\%PROJECT_NAME%.dll x64\release\%PROJECT_NAME%64.dll sample.reg
+) else (
+echo.
+echo DebugBreak found in source code. Fix it or die.
+if exist ..\%PROJECT_NAME%.zip del ..\%PROJECT_NAME%.zip
+if exist ..\%PROJECT_NAME%-src.zip del ..\%PROJECT_NAME%-src.zip
+)
 
 popd
 
